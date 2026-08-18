924edd9b47a6301c52f914d1ea8fa5025f1bb620
Ashu Jha <ashujha009322@gmail.com>
2026-08-18 13:09:09 +0530

    slight modification in database settings to make deployement easy

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,19 @@
-// lib/auth.ts
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "@better-auth/prisma-adapter";
 import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql", // Matches Supabase
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
